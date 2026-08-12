daf6d0b34097578d91ed31a60d5e7efc000718ef
KANIKA VERMA <vermakanika002@gmail.com>
2026-08-12 19:26:21 +0530

    Fix import path casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
-import TodoItems from "./components/TOdoItems";
+import TodoItems from "./components/TodoItems";
 import "./App.css";
 import WelcomeMessage from "./components/WelcomeMessage";
 import { useState } from "react";
